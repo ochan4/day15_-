b0b5cd0b74066a48d5a9b8a935b8281d6897302d
tarena <tarena@tarenadeimac-15.local>
2016-04-22 16:39:19 +0800

    Initial Commit

---
+++ 反归档/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  反归档
+//
+//  Created by tarena on 16/4/22.
+//  Copyright © 2016年 tarena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
